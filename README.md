12e0372b2d2fd313b6cdd09017542146d734ead1
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-03-26 01:30:32 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Password-Generator-H3
+## Description
+
+- Homework one was to create a password generator.
+
+- We had to manipulate the .js file to make the password generator work.
+
+## Usage
+
+FOR AN employee with access to sensitive data
+I WILL to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+GIVEN I need a new, secure password
+WHEN they click the button to generate a password
+THEN they am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN they select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN they choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN they confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN they answer each prompt
+THEN the input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+Screenshot:
+![alt tag] (https://user-images.githubusercontent.com/99215212/160227689-0ee24433-92c5-4cc0-9357-19f8c329b37a.png "screenshot")
+
+To view the new password generator, please click: 
---
+++ script.js
@@ -0,0 +1,150 @@
+//Array of password length
+var passwordLength = 8;
+
+//Array of picking the password
+var pickingArr = [];
+
+// Array of uppercase characters to be included in password
+var uppers = [
+  "A", 
+  "B", 
+  "C", 
+  "D", 
+  "E", 
+  "F", 
+  "G",
+  "H", 
+  "I", 
+  "J", 
+  "K", 
+  "L", 
+  "M", 
+  "N", 
+  "O", 
+  "P", 
+  "Q", 
+  "R", 
+  "S",
+  "T", 
+  "U", 
+  "V", 
+  "W", 
+  "X", 
+  "Y", 
+  "Z"
+];
+
+// Array of lowercase characters to be included in password
+var lowers = [
+  "a", 
+  "b", 
+  "c", 
+  "d", 
+  "e", 
+  "f", 
+  "g",
+  "h", 
+  "i", 
+  "j", 
+  "k", 
+  "l", 
+  "m", 
+  "n", 
+  "o", 
+  "p", 
+  "q", 
+  "r", 
+  "s",
+  "t", 
+  "u", 
+  "v", 
+  "w", 
+  "x", 
+  "y", 
+  "z"
+];
+
+// Array of special characters to be included in password
+var specials = [
+  "/", 
+  ";", 
+  "!", 
+  "@", 
+  "#", 
+  "$", 
+  "%", 
+  "&", 
+  "*", 
+  "~"
+];
+
+// Array of numberss to be included in password
+var numbers = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9"
+];
+
+//generating the actual password
+var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", writePassword);
+
+//getting the password
+function writePassword() {
+  var goodPrompts = getPrompts();
+  var passwordText = document.querySelector("#password");
+
+  if(goodPrompts){
+  var thePassword = generatePassword();
+  passwordText.value = thePassword;
+  } else {
+    passwordText.value = "";
+  }
+}
+
+//creating the password
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < passwordLength; i++){
+    var randomLetter = Math.floor(Math.random() * pickingArr.length);
+    password = password + pickingArr[randomLetter];
+  }
+  return password;
+}
+
+//all of the prompts & confirm 
+function getPrompts() {
+  pickingArr = [];
+  passwordLength = parseInt(prompt("How long do you want a password? Please choose between 8 and 128."));
+
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a number between 8 and 128!");
+    return false;
+  }
+
+  if(confirm("Do you want to include special characters?")) {
+    pickingArr = pickingArr.concat(specials);
+  }
+
+  if(confirm("Do you want to include Uppercase characters?")) {
+    pickingArr = pickingArr.concat(uppers);
+  }
+
+  if(confirm("Do you want to include Lowercase characters?")) {
+    pickingArr = pickingArr.concat(lowers);
+  }
+
+  if(confirm("Do you want to include number characters?")) {
+    pickingArr = pickingArr.concat(numbers);
+  }
+
+  return true;
+}
